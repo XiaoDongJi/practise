1c6651e1ffff74c511fca5ab60f561b01a837a50
JIXD <jixd5@ziroom.com>
2018-04-06 23:12:30 +0800

    1.nio

---
+++ src/main/java/com/cckeep/jdk/pv/BoundedBuffer.java
@@ -0,0 +1,57 @@
+package com.cckeep.jdk.pv;
+
+import java.util.concurrent.Semaphore;
+
+/**
+ * 基于信号量得有界缓存
+ */
+public class BoundedBuffer<E> {
+    //元素个数 剩余空间
+    private final Semaphore availableItems,availableSpaces;
+    //存放元素
+    private final E[] items;
+    //放入位置  移除位置
+    private int putPosition = 0,takePosition = 0;
+
+    public BoundedBuffer(int capacity) {
+        availableItems = new Semaphore(0);
+        availableSpaces = new Semaphore(capacity);
+        items = (E[]) new Object[capacity];
+    }
+
+    public boolean isEmpty(){
+        return availableItems.availablePermits() == 0;
+    }
+
+    public boolean isFull(){
+        return availableSpaces.availablePermits() == 0;
+    }
+
+    public void put(E x) throws InterruptedException {
+        availableSpaces.acquire();
+        doInsert(x);
+        availableItems.release();
+    }
+
+    public E take() throws InterruptedException {
+        availableItems.acquire();
+        E item = doExtract();
+        availableSpaces.release();
+        return item;
+    }
+
+    private synchronized void doInsert(E x) {
+        int i = putPosition;
+        items[i] = x;
+        putPosition = (++i == items.length) ? 0 : i;
+    }
+
+    private synchronized E doExtract(){
+        int i = takePosition;
+        E x = items[i];
+        items[i] = null;
+        takePosition = (++i == items.length) ? 0 : i;
+        return x;
+    }
+
+}
---
+++ src/main/java/com/cckeep/jdk/threadpool/TimingThreadPool.java
@@ -0,0 +1,36 @@
+package com.cckeep.jdk.threadpool;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.logging.Logger;
+
+public class TimingThreadPool extends ThreadPoolExecutor{
+
+    private final ThreadLocal<Long> startTime = new ThreadLocal<Long>();
+    private final Logger log = Logger.getLogger("TimingThreadPool");
+    private final AtomicLong numTasks = new AtomicLong();
+    private final AtomicLong totalTime = new AtomicLong();
+
+
+
+    public TimingThreadPool(int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit, BlockingQueue<Runnable> workQueue) {
+        super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue);
+    }
+
+
+    @Override
+    protected void beforeExecute(Thread t, Runnable r) {
+        super.beforeExecute(t, r);
+        startTime.set(System.nanoTime());
+    }
+
+    @Override
+    protected void afterExecute(Runnable r, Throwable t) {
+        long endTime = System.nanoTime();
+        long taskTime = endTime - startTime.get();
+        numTasks.incrementAndGet();
+        super.afterExecute(r, t);
+    }
+}
