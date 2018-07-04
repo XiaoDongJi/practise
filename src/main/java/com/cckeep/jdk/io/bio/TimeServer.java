b9a1adad0eac9622947dff86a47db0b0a932aaf9
JIXD <jixd5@ziroom.com>
2018-07-04 19:57:40 +0800

    1.提交

---
+++ src/main/java/com/cckeep/jdk/io/bio/TimeServer.java
@@ -0,0 +1,30 @@
+package com.cckeep.jdk.io.bio;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class TimeServer {
+
+    public static void main(String[] args) throws IOException {
+        int port = 8080;
+        ServerSocket server = null;
+        try {
+            server = new ServerSocket(port);
+            System.out.println("the time server is start in port:"+port);
+            Socket socket = null;
+            while (true){
+                socket = server.accept();
+                new Thread(new TimeServerHandler(socket)).start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            if (server != null){
+                server.close();
+                server = null;
+            }
+        }
+    }
+
+}
