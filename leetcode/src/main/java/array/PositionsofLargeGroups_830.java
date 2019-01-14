033b4e6669bde76ccaf126f84706a485e396e4e1
jixd <jixdms@gmail.com>
2019-01-15 07:45:34 +0800

    tijiao

--- leetcode/src/main/java/array/PositionsofLargeGroups_830.java
+++ leetcode/src/main/java/array/PositionsofLargeGroups_830.java
@@ -1,4 +1,49 @@
 package array;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * In a string S of lowercase letters, these letters form consecutive groups of the same character.
+ *
+ * For example, a string like S = "abbxxxxzyy" has the groups "a", "bb", "xxxx", "z" and "yy".
+ *
+ * Call a group large if it has 3 or more characters.  We would like the starting and ending positions of every large group.
+ *
+ * The final answer should be in lexicographic order.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: "abbxxxxzzy"
+ * Output: [[3,6]]
+ * Explanation: "xxxx" is the single large group with starting  3 and ending positions 6.
+ * Example 2:
+ *
+ * Input: "abc"
+ * Output: []
+ * Explanation: We have "a","b" and "c" but no large group.
+ * Example 3:
+ *
+ * Input: "abcdddeeeeaabbbcd"
+ * Output: [[3,5],[6,9],[12,14]]
+ */
 public class PositionsofLargeGroups_830 {
+
+
+    public List<List<Integer>> largeGroupPositions(String S) {
+        List<List<Integer>> lists = new ArrayList<>();
+        if (S.length() < 3){
+            return lists;
+        }
+
+        for (int i = 0; i < S.length(); i++) {
+            S.charAt(i);
+
+        }
+        return null;
+    }
+
+
 }
