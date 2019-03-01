590d13e1e7eb67d15f57e889f1ea900a902b61cb
JIXD <jixd5@ziroom.com>
2019-03-01 16:31:02 +0800

    commit

---
+++ simple-practise/src/main/java/com/cckeep/reactor/MonoTest.java
@@ -0,0 +1,58 @@
+package com.cckeep.reactor;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+
+import java.util.Optional;
+
+/**
+ * <p>TODO</p>
+ *
+ * @author jixd
+ * @Date Created in 2019年02月25日 20:25
+ */
+public class MonoTest {
+
+    public static void main(String[] args) {
+
+        Mono.fromSupplier(() -> "Hello").subscribe(System.out::println);
+        Mono.justOrEmpty(Optional.of("Hello")).subscribe(System.out::println);
+        Mono.create(sink -> sink.success("Hello")).subscribe(System.out::println);
+
+        Flux.range(1, 100).buffer(20).subscribe(System.out::println);
+        Flux.range(1, 10).bufferUntil(i -> i % 2 == 0).subscribe(System.out::println);
+        Flux.range(1, 10).bufferWhile(i -> i % 2 == 0).subscribe(System.out::println);
+
+        Flux.just("a", "b")
+                .zipWith(Flux.just("c", "d"))
+                .subscribe(System.out::println);
+        Flux.just("a", "b")
+                .zipWith(Flux.just("c", "d"), (s1, s2) -> String.format("%s-%s", s1, s2))
+                .subscribe(System.out::println);
+
+
+        Flux.range(1, 1000).take(10).subscribe(System.out::println);
+        Flux.range(1, 1000).takeLast(10).subscribe(System.out::println);
+        Flux.range(1, 1000).takeWhile(i -> i < 10).subscribe(System.out::println);
+        Flux.range(1, 1000).takeUntil(i -> i == 10).subscribe(System.out::println);
+
+
+        Flux.range(1, 100).reduce((x, y) -> x + y).subscribe(System.out::println);
+        Flux.range(1, 100).reduceWith(() -> 100, (x, y) -> x + y).subscribe(System.out::println);
+
+
+        Flux.create(sink -> {
+            sink.next(Thread.currentThread().getName());
+            sink.complete();
+        })
+                .publishOn(Schedulers.single())
+                .map(x -> String.format("[%s] %s", Thread.currentThread().getName(), x))
+                .publishOn(Schedulers.elastic())
+                .map(x -> String.format("[%s] %s", Thread.currentThread().getName(), x))
+                .subscribeOn(Schedulers.parallel())
+                .toStream()
+                .forEach(System.out::println);
+    }
+
+}
