9d8af4269c61f2aee2c0aee4b7ad51df88c82ce3
jixd <jixd5@ziroom.com>
2021-03-20 12:07:13 +0800

    f

--- simple-practise/src/main/java/com/cckeep/test/Test.java
+++ simple-practise/src/main/java/com/cckeep/test/Test.java
@@ -1,7 +1,9 @@
 package com.cckeep.test;
 
+import com.sun.management.VMOption;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import sun.management.HotSpotDiagnostic;
 
 import java.math.BigDecimal;
 import java.util.Arrays;
@@ -15,7 +17,11 @@ public class Test {
 
     public static void main(String[] args) {
 
-        System.out.println(BigDecimal.valueOf(0).compareTo(BigDecimal.ZERO));
+        HotSpotDiagnostic mxBean = new HotSpotDiagnostic();
+        List<VMOption> diagnosticVMOptions = mxBean.getDiagnosticOptions();
+        for (VMOption vmOption:diagnosticVMOptions){
+            System.out.println(vmOption.getName() + " = " + vmOption.getValue());
+        }
 
     }
 }
